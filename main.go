b6000bb365acb8966f9725741515cb2160a8c063
huangjq <jq.go@qq.com>
2020-06-16 13:49:31 +0800

    feat: xlsx2csv.exe [flags] <dir-to-be-read> <dir-to-output-to>

--- main.go
+++ main.go
@@ -9,12 +9,69 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"github.com/tealeg/xlsx/v3"
 	"io"
 	"log"
 	"os"
-
-	"github.com/tealeg/xlsx/v3"
+	"path/filepath"
+	"strings"
 )
+
+func generateCSVFromXLSXFile2(outDir string, excelFileName string, sheetIndex int, csvOpts csvOptSetter) error {
+	xlFile, err := xlsx.OpenFile(excelFileName)
+	if err != nil {
+		return err
+	}
+	sheetLen := len(xlFile.Sheets)
+	switch {
+	case sheetLen == 0:
+		return errors.New("This XLSX file contains no sheets.")
+	case sheetIndex >= sheetLen:
+		return fmt.Errorf("No sheet %d available, please select a sheet between 0 and %d\n", sheetIndex, sheetLen-1)
+	}
+	sheet := xlFile.Sheets[sheetIndex]
+
+	f := sheet.Name + ".csv"
+	p := filepath.Join(outDir, f)
+	w, e := os.Create(p)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer func() {
+		if closeErr := w.Close(); closeErr != nil {
+			log.Fatal(closeErr)
+		}
+	}()
+
+	cw := csv.NewWriter(w)
+	if csvOpts != nil {
+		csvOpts(cw)
+	}
+	var vals []string
+	err = sheet.ForEachRow(func(row *xlsx.Row) error {
+		if row != nil {
+			vals = vals[:0]
+			err := row.ForEachCell(func(cell *xlsx.Cell) error {
+				str, err := cell.FormattedValue()
+				if err != nil {
+					return err
+				}
+				vals = append(vals, str)
+				return nil
+			})
+			if err != nil {
+				return err
+			}
+		}
+		cw.Write(vals)
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	cw.Flush()
+	return cw.Error()
+}
 
 func generateCSVFromXLSXFile(w io.Writer, excelFileName string, sheetIndex int, csvOpts csvOptSetter) error {
 	xlFile, err := xlsx.OpenFile(excelFileName)
@@ -63,42 +120,40 @@ type csvOptSetter func(*csv.Writer)
 
 func main() {
 	var (
-		outFile    = flag.String("o", "-", "filename to output to. -=stdout")
+		//outDir     = flag.String("o", ".", "dir to output to")
 		sheetIndex = flag.Int("i", 0, "Index of sheet to convert, zero based")
-		delimiter  = flag.String("d", ";", "Delimiter to use between fields")
+		delimiter  = flag.String("d", "\t", "Delimiter to use between fields")
 	)
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, `%s
-	dumps the given xlsx file's chosen sheet as a CSV,
-	with the specified delimiter, into the specified output.
-
-Usage:
-	%s [flags] <xlsx-to-be-read>
-`, os.Args[0], os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s [flags] <dir-to-be-read> <dir-to-output-to>\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 
 	flag.Parse()
-	if flag.NArg() != 1 {
+	if flag.NArg() != 2 {
 		flag.Usage()
 		os.Exit(1)
 	}
-	out := os.Stdout
-	if !(*outFile == "" || *outFile == "-") {
-		var err error
-		if out, err = os.Create(*outFile); err != nil {
-			log.Fatal(err)
-		}
-	}
-	defer func() {
-		if closeErr := out.Close(); closeErr != nil {
-			log.Fatal(closeErr)
-		}
-	}()
 
-	if err := generateCSVFromXLSXFile(out, flag.Arg(0), *sheetIndex,
-		func(cw *csv.Writer) { cw.Comma = ([]rune(*delimiter))[0] },
-	); err != nil {
-		log.Fatal(err)
-	}
+	filepath.Walk(flag.Arg(0), func(path string, info os.FileInfo, err error) error {
+		//忽略目录
+		if info.IsDir() {
+			return nil
+		}
+		//是不是xlsx
+		if strings.Compare(filepath.Ext(path), ".xlsx") != 0 {
+			return nil
+		}
+
+		outDir := flag.Arg(1)
+		os.MkdirAll(outDir, 0755)
+
+		if err := generateCSVFromXLSXFile2(outDir, path, *sheetIndex, func(cw *csv.Writer) {
+			cw.Comma = ([]rune(*delimiter))[0]
+		}); err != nil {
+			//log.Fatal(err)
+			log.Println(err)
+		}
+		return nil
+	})
 }
